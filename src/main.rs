58116b1b925d2552244173a9e32f3c4b34c2d355
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2024-12-23 10:46:56 +0530

    enhance main function with variable manipulation and conditional checks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x: i8 = 5;
+    println!("{}", x);
+
+    for i in 0..10 {
+        x = x + i;
+    }
+    println!("{}", x);
+    let is_male = true;
+    let is_legal = true;
+
+    if is_male{
+        println!{"{}", "Hee is a male!"}
+    }
+    if is_legal{
+        println!{"{}", "He is legal!"}
+    }
+    if is_male && is_legal{
+        println!{"{}", "He is a legal Male!"}
+    }
+
+    let mut greeting = String :: from("Hello, World!");
+    println!("{}", greeting.clone() + "YoYo");
+    println!("{}", greeting);
+    let char1 = greeting.chars().nth(0);    
+    println!("{}", char1.unwrap());
 }
